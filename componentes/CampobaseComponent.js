1f845914dbcb3bdaeb3f2fc1fbd835c6f4e6a9ae
Ivan <navascues.131644@e.unavarra.es>
2026-04-16 19:00:38 +0200

    Componentes funcionales en React Native Añadido subcomponente para renderizado de tarjetas individuales

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import Calendario from './CalendarioComponent';
 import { EXCURSIONES } from '../comun/excursiones';
+import { View } from 'react-native';
+import DetalleExcursion from './DetalleExcursionComponent';
 class Campobase extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            excursiones: EXCURSIONES
+            excursiones: EXCURSIONES,
+            seleccionExcursion: null
         };
+    }
+    onSeleccionExcursion(excursionId) {
+        this.setState({ seleccionExcursion: excursionId })
     }
     render() {
 
         return (
-            <Calendario excursiones={this.state.excursiones} />
+            <View style={{ flex: 1 }}>
+                <DetalleExcursion excursion={this.state.excursiones.find((excursion) => excursion.id === this.state.seleccionExcursion)} />
+                <Calendario excursiones={this.state.excursiones} onPress={(
+                    excursionId) => this.onSeleccionExcursion(excursionId)} />
+            </View>
         );
     }
 }
