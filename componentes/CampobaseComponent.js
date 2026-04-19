3e3124ece6e5cc5510d9f9e573ebbeac3fbbf866
Ivan <navascues.131644@e.unavarra.es>
2026-04-19 19:05:32 +0200

    Botones o Iconos Varios hitos alcanzados: - Añadida lista de comentarios para cada excursión (Card de comentario + Flatlist de comentarios dentro) - Nuevo botón de favoritos para cada excursión (aunque no se guarda el estado de la lista de favoritos) - Modificado menú principal; accesible con botón desde Campobase + iconos para las opciones

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -2,17 +2,55 @@ import { Component } from 'react';
 import Calendario from './CalendarioComponent';
 import DetalleExcursion from './DetalleExcursionComponent';
 import { EXCURSIONES } from '../comun/excursiones';
-import { Platform, View } from 'react-native';
+import { View, Platform, StyleSheet, Image, Text, Pressable, SafeAreaView } from 'react-native';
 import Constants from 'expo-constants';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import Home from './HomeComponent';
 import Contacto from './ContactoComponent';
 import QuienesSomos from './QuienesSomosComponent';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+
+function BotonMenu(props) {
+    return (
+        <Pressable
+            onPress={props.onPress}
+            hitSlop={8}
+        >
+            <MaterialCommunityIcons
+                name="menu"
+                size={40}
+                color={Platform.OS === 'ios' ? '#015afc' : 'white'}
+            />
+        </Pressable>
+    );
+}
+function CustomDrawerContent(props) {
+    return (
+        <DrawerContentScrollView {...props}>
+            <SafeAreaView style={styles.container} edges={['left', 'right',
+                'bottom']}>
+                <View style={styles.drawerHeader}>
+                    <View style={styles.drawerHeaderImageContainer}>
+                        <Image
+                            source={require('./imagenes/logo.png')}
+                            style={styles.drawerImage}
+                        />
+                    </View>
+                    <View style={styles.drawerHeaderTextContainer}>
+                        <Text style={styles.drawerHeaderText}>Gaztaroa</Text>
+                    </View>
+                </View>
+                <DrawerItemList {...props} />
+            </SafeAreaView>
+        </DrawerContentScrollView>
+    );
+}
+
 
 class Campobase extends Component {
     constructor(props) {
@@ -21,24 +59,33 @@ class Campobase extends Component {
             excursiones: EXCURSIONES,
         };
     }
+    menuHeaderOptions = (title, navigation) => ({
+        title,
+        headerLeft: () => (
+            <BotonMenu
+                onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+            />
+        ),
+    });
 
     HomeNavegador = () => {
-        return (<Stack.Navigator
-            initialRouteName="Home"
-            screenOptions={{
-                headerTintColor: '#fff',
-                headerStyle: { backgroundColor: '#015afc' },
-                headerTitleStyle: { color: '#fff' },
-            }}
-        >
-            <Stack.Screen
-                name="Home"
-                component={Home}
-                options={{
-                    title: 'Campo Base',
+        return (
+            <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                    headerTintColor: '#fff',
+                    headerStyle: { backgroundColor: '#015afc' },
+                    headerTitleStyle: { color: '#fff' },
                 }}
-            />
-        </Stack.Navigator>
+            >
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={({ navigation }) =>
+                        this.menuHeaderOptions('Campo Base', navigation)
+                    }
+                />
+            </Stack.Navigator>
         );
     };
     QuienesSomosNavegador = () => {
@@ -119,10 +166,12 @@ class Campobase extends Component {
         </Stack.Navigator>
         );
     };
+
     DrawerNavegador = () => {
         return (
             <Drawer.Navigator
                 initialRouteName="Campo base"
+                drawerContent={(props) => <CustomDrawerContent {...props} />}
                 screenOptions={{
                     headerShown: false,
                     drawerStyle: {
@@ -133,23 +182,60 @@ class Campobase extends Component {
                 <Drawer.Screen
                     name="Campo base"
                     component={this.HomeNavegador}
+                    options={{
+                        drawerIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="home"
+                                color={color}
+                                size={size}
+                            />
+                        ),
+                    }}
                 />
                 <Drawer.Screen
                     name="Quienes somos"
                     component={this.QuienesSomosNavegador}
+                    options={{
+                        drawerIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="information"
+                                color={color}
+                                size={size}
+                            />
+                        ),
+                    }}
                 />
                 <Drawer.Screen
                     name="Calendario"
                     component={this.CalendarioNavegador}
+                    options={{
+                        drawerIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="calendar"
+                                color={color}
+                                size={size}
+                            />
+                        ),
+                    }}
                 />
                 
                 <Drawer.Screen
                     name="Contacto"
                     component={this.ContactoNavegador}
+                    options={{
+                        drawerIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="contacts"
+                                color={color}
+                                size={size}
+                            />
+                        ),
+                    }}
                 />
             </Drawer.Navigator>
         );
     };
+
     render() {
         return (
             <NavigationContainer>
@@ -163,5 +249,37 @@ class Campobase extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#015afc',
+        height: 100,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 8,
+    },
+    drawerHeaderImageContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    drawerHeaderTextContainer: {
+        flex: 2,
+        justifyContent: 'center',
+    },
+    drawerHeaderText: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    drawerImage: {
+        width: 80,
+        height: 60,
+        resizeMode: 'contain',
+    },
+});
 
 export default Campobase;
--- componentes/DetalleExcursionComponent.js
+++ componentes/DetalleExcursionComponent.js
@@ -1,7 +1,37 @@
 import { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Card, Text } from 'react-native-paper';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import { COMENTARIOS } from '../comun/comentarios';
+import { Card, Text, Divider, IconButton } from 'react-native-paper';
 import { EXCURSIONES } from '../comun/excursiones';
+import { FlatList } from 'react-native-gesture-handler';
+
+function RenderComentario(props) {
+    const comentarios = props.comentarios;
+    if (comentarios != null) {
+        return (
+            <Card style={styles.card}>
+                <Card.Title
+                    title="Comentarios"
+                    titleStyle={styles.titulo}
+                    style={styles.cardTitle}
+                />
+                <Card.Content>
+                    <FlatList
+                        data={comentarios}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item }) => (
+                            <Text style={styles.descripcion}>
+                                {item.comentario} - {item.autor} - {item.dia}
+                            </Text>
+                        )}
+                    />
+                </Card.Content>
+            </Card>
+        );
+    }
+
+    return <View />
+}
 
 function RenderExcursion(props) {
     const excursion = props.excursion;
@@ -22,6 +52,18 @@ function RenderExcursion(props) {
                     <Text style={styles.descripcion}>
                         {excursion.descripcion}
                     </Text>
+
+                    <View style={styles.iconoContainer}>
+                        <IconButton
+                            icon={props.favorita ? 'heart' : 'heart-outline'}
+                            size={28}
+                            onPress={() =>
+                                props.favorita
+                                    ? console.log('La excursión ya se encuentra entre las favoritas')
+                                    : props.onPress()
+                            }
+                        />
+                    </View>
                 </Card.Content>
             </Card>
         );
@@ -34,20 +76,42 @@ class DetalleExcursion extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            excursiones: EXCURSIONES
+            excursiones: EXCURSIONES,
+            comentarios: COMENTARIOS,
+            favoritos: []
         };
+    }
+    marcarFavorito(excursionId) {
+        this.setState({
+            favoritos: this.state.favoritos?.concat(excursionId)
+        });
     }
 
     render() {
         const { excursionId } = this.props.route.params;
+        return (
+            <ScrollView>
+                <RenderExcursion
+                    excursion={this.state.excursiones[+excursionId]}
+                    favorita={this.state.favoritos.some(el => el === excursionId)}
+                    onPress={() => this.marcarFavorito(excursionId)}
+                />
+                <RenderComentario
+                    comentarios={this.state.comentarios.filter((comentario) => comentario.excursionId === excursionId)}
+                />
+            </ScrollView>
 
-        return <RenderExcursion excursion={this.state.excursiones[+excursionId]} />;
+        );
     }
 }
 
 const styles = StyleSheet.create({
     card: {
         margin: 8,
+    },
+    iconoContainer: {
+        alignItems: 'center',
+        marginBottom: 8,
     },
     image: {
         marginHorizontal: 0,
